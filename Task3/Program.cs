8b75edf1ef96c71e440f276b7c86d4c407414941
NadiaBrin <nadiabrin@gmail.com>
2022-09-10 05:18:51 +0300

    Написать программу копирования массива

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу копирования массива
+
+int[] array = new int[8];
+int arrayCopy;
+
+Console.Write("Оригинал ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+Console.Write("   Копия ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+}
